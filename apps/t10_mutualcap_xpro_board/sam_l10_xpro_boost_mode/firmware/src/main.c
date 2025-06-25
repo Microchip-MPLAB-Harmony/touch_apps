64c60477f9e3c4aeb50340a5ce98071ff77a22e4
Abirami Anbalagan <abirami.anbalagan@microchip.com>
2025-06-25 16:18:22 +0530

    Trustzone and Boost mode examples- MH3-113608

---
+++ apps/t10_mutualcap_xpro_board/sam_l10_xpro_boost_mode/firmware/src/main.c
@@ -0,0 +1,64 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+#include <string.h>
+#include "led_driver.h"
+
+
+// *****************************************************************************
+// Function Prototypes
+// *****************************************************************************
+extern volatile uint8_t measurement_done_touch;
+void APP_I2CCallback(uintptr_t context);
+void Update_LED_Display(void);
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main(void) {
+    /* Initialize all modules */
+    SYS_Initialize(NULL);
+
+    while (1) {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks();
+        touch_process();
+        if (measurement_done_touch == 1) 
+        {
+            Update_LED_Display();
+        }
+    }
+    /* Execution should not come here during normal operation */
+    return ( EXIT_FAILURE);
+}
+
+
+
